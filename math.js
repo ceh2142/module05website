75c0314265e175421c0197265b62527c7da840a1
ceh2142 <chantelle.hudspeth@bellevuecollege.edu>
2021-05-26 20:51:14 -0700

    label

--- math.js
+++ math.js
@@ -1,7 +1,7 @@
 var table = 5;             // Unit of table
 var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var msg = 'Multiplication Table';              // Message
 
 if (operator === 'addition') {
   // Do addition
