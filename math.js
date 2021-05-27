57f4ca5d5786e761b2811bfdb195014519234b9b
ceh2142 <chantelle.hudspeth@bellevuecollege.edu>
2021-05-26 20:41:18 -0700

    java

--- math.js
+++ math.js
@@ -1,5 +1,28 @@
+var table = 5;             // Unit of table
+var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
-var msg = 'Multiplication Table';              // Message
+var msg = '';              // Message
+
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
+
+var i = 1;       // Set counter to 1
+var msg = '';    // Message
 
 // Store 5 times table in a variable
 while (i < 10) {
