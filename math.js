b6c0011b59a090642d31a0d3c0889ccda71edd86
ceh2142 <chantelle.hudspeth@bellevuecollege.edu>
2021-05-26 18:47:29 -0700

    table numbers

--- math.js
+++ math.js
@@ -1,7 +1,7 @@
 var table = i;             // Unit of table
 var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
-var msg = 'Multiplication Table';              // Message
+var msg = '';              // Message
 
 if (operator === 'addition') {
   // Do addition
@@ -20,15 +20,3 @@ if (operator === 'addition') {
 // Write the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;
-
-var i = 1;       // Set counter to 1
-var msg = '';    // Message
-
-// Store 5 times table in a variable
-do {
-  msg += i + ' x 5 = ' + (i * 5) + '<br />';
-  i++;
-} while (i < 1); 
-// Note how this is already 1 and it still runs
-
-document.getElementById('answer').innerHTML = msg;
